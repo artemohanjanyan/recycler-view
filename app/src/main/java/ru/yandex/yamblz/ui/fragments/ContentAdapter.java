53cf09637d3f7f973aa3ce974fd1722f9ef5b36f
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-07-24 20:51:25 +0300

    tasks 1-3

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentAdapter.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentAdapter.java
@@ -2,12 +2,14 @@ package ru.yandex.yamblz.ui.fragments;
 
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -34,10 +36,14 @@ class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ContentHolder> 
     }
 
     private Integer createColorForPosition(int position) {
-        if (position >= colors.size()) {
+        while (position >= colors.size()) {
             colors.add(Color.rgb(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255)));
         }
         return colors.get(position);
+    }
+
+    ItemTouchHelper.Callback buildItemTouchHelperCallback() {
+        return new ItemTouchHelperCallback();
     }
 
     static class ContentHolder extends RecyclerView.ViewHolder {
@@ -50,4 +56,38 @@ class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ContentHolder> 
             ((TextView) itemView).setText("#".concat(Integer.toHexString(color).substring(2)));
         }
     }
+
+    private class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            return makeMovementFlags(ItemTouchHelper.UP | ItemTouchHelper.DOWN |
+                            ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT,
+                    ItemTouchHelper.RIGHT);
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView,
+                              RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            int from = viewHolder.getAdapterPosition(), to = target.getAdapterPosition();
+            if (from < to) {
+                for (int i = from; i < to; ++i) {
+                    Collections.swap(ContentAdapter.this.colors, i, i + 1);
+                }
+            } else {
+                for (int i = from; i > to; --i) {
+                    Collections.swap(ContentAdapter.this.colors, i, i - 1);
+                }
+            }
+            ContentAdapter.this.notifyItemMoved(from, to);
+            return true;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+            ContentAdapter.this.colors.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
 }
--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -3,9 +3,13 @@ package ru.yandex.yamblz.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,8 +18,18 @@ import ru.yandex.yamblz.R;
 
 public class ContentFragment extends BaseFragment {
 
+    private static final String COLUMN_N = "column number";
+
     @BindView(R.id.rv)
     RecyclerView rv;
+
+    private GridLayoutManager layoutManager;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @NonNull
     @Override
@@ -26,7 +40,40 @@ public class ContentFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        rv.setLayoutManager(new LinearLayoutManager(getContext()));
-        rv.setAdapter(new ContentAdapter());
+        int columnN = 1;
+        if (savedInstanceState != null) {
+            columnN = savedInstanceState.getInt(COLUMN_N, 1);
+        }
+        layoutManager = new GridLayoutManager(getContext(), columnN);
+        rv.setLayoutManager(layoutManager);
+        ContentAdapter adapter = new ContentAdapter();
+        rv.setAdapter(adapter);
+        new ItemTouchHelper(adapter.buildItemTouchHelperCallback()).attachToRecyclerView(rv);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_menu, menu);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(COLUMN_N, layoutManager.getSpanCount());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_add:
+                layoutManager.setSpanCount(layoutManager.getSpanCount() + 1);
+                break;
+            case R.id.menu_item_remove:
+                layoutManager.setSpanCount(Math.max(1, layoutManager.getSpanCount() - 1));
+                break;
+        }
+        layoutManager.requestLayout();
+        return true;
     }
 }
